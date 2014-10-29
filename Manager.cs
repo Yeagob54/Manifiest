b28d25be666e355568e0989f74c1680671fbd29e
Santiago Dopazo Hilario <santidopazo@gmail.com>
2014-10-29 10:39:54 +0100

    Normalizando...

--- Manager.cs
+++ Manager.cs
@@ -1,5 +1,5 @@
 /******************************************************************************
- * manager.cs
+ * Manager.cs
  * 
  * Clase global, instanciada en temp, para controlar la cantidad de manifestantes, 
  * peatones, coches y policias. 
@@ -14,8 +14,8 @@ using System.Collections.Generic;
 
 public class Manager : MonoBehaviour {
 
-	//Puntero a si mismo, para instanciarse
-	public static manager temp;
+	//Puntero a si mismo, para acceder a la instancia
+	public static Manager temp;
 
 	//Listado de objetos-unidad del juego 
 	public List<GameObject> unidades = new List<GameObject>();
@@ -41,100 +41,104 @@ public class Manager : MonoBehaviour {
 	//Velociadad a la que transcurre el tiempo en el juego
 	public float velocidadTiempo = 1;
 	
+
 	// Inicializamos
 	void Start () 	{
-	  //Creamos en temp una instancia de la clase. 
 		temp = this;	
 	}
 	
+
 	/************************************************************
 	 * AÑADIR Y QUITAR MANIFESTANTES, COCHES, PEATONES Y POLICIAS
 	 * **********************************************************/
-	public void addManifest()
+	public void AddManifest()
 	{
 		numeroDeManifestantes ++;
 	}
 	
-	public void lessManifest()
+	public void LessManifest()
 	{
 		numeroDeManifestantes --;
 	}
 	
-	public int getManifest()
+	public int GetManifest()
 	{
 		return numeroDeManifestantes;
 	}
 
-	public void addPolicias()
+	public void AddPolicias()
 	{
 		numeroDePolicias ++;
 	}
 	
-	public void lessPolicias()
+	public void LessPolicias()
 	{
 		numeroDePolicias --;
 	}
 	
-	public int getPolicias()
+	public int GetPolicias()
 	{
 		return numeroDePolicias;
 	}
 	
-	public void addCoches()
+	public void AddCoches()
 	{
 		numeroDeCoches ++;
 	}
 	
-	public void lessCoches()
+	public void LessCoches()
 	{
 		numeroDeCoches --;
 	}
 	
-	public int getCoches()
+	public int GetCoches()
 	{
 		return numeroDeCoches;
 	}
 
-	public void addPeatones()
+	public void AddPeatones()
 	{
 		numeroDePeatones ++;
 	}
 	
-	public void lessPeatones()
+	public void LessPeatones()
 	{
 		numeroDePeatones --;
 	}
 	
-	public int getPeatones()
+	public int GetPeatones()
 	{
 		return numeroDeCoches;
 	}
 
+
 	/*****************************************************************
 	 *    MODIFICACION Y SOLICITAR VALOR DE LAS BARRAS DE OBJETIVO
 	 * ***************************************************************/
-	public float incRepercusion(float cuanto) {
+	public void IncRepercusion(float cuanto) {
 		repercusionMediatica += cuanto;
 	}
 
-	public float getRepercusion() {
+	public float GetRepercusion() {
 		return repercusionMediatica;
 	}
 
-	public float incAmbiente(float cuanto) {
+	public void IncAmbiente(float cuanto) {
 		ambienteManifestacion += cuanto;
 	}
 	
-	public float getAmbiente() {
+	public float GetAmbiente() {
 		return ambienteManifestacion;
 	}
 
-	public float incConciencia(float cuanto) {
+	public void IncConciencia(float cuanto) {
 		nivelConcienciaLocal += cuanto;
 	}
 	
-	public float getConciencia() {
+	public float GetConciencia() {
 		return nivelConcienciaLocal;
 	}
+
+
 
 }

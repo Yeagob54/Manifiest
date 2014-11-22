a2359cdf86c07efed2f5238db2e1ad79febd502c
Santiago Dopazo Hilario <santidopazo@gmail.com>
2014-11-22 18:31:55 +0100

    Update Manager.cs

--- Manager.cs
+++ Manager.cs
@@ -14,8 +14,20 @@ using System.Collections.Generic;
 
 public class Manager : MonoBehaviour {
 
+	//Constantes del juego
+	const int MINIMO_MANIFESTANTES = 5;
+	
 	//Modificacion de interfaces y controles para modo tablet
 	public bool versionTablet = false;
+
+	//Referencia al lider de la manifestacion
+	public GameObject liderAlpha;
+
+	//Referencia al punto de encuentro inicial
+	public GameObject puntoReunion;
+
+	//Referencia a las cámaras
+	public Camera mainMenuCamera;
 
 	//Listado de las unidades-manifestantes del juego 
 	public List<GameObject> unidades = new List<GameObject>();
@@ -27,10 +39,13 @@ public class Manager : MonoBehaviour {
 	public List<string> objetivos = new List<string>();
 
 	//Control de la cantidad de manifestantes, peatones, policias y coches
-	private int numeroDeManifestantes=0, numeroDePeatones=0, numeroDePolicias=0, numeroDeCoches=0;	
+	private int numeroDeManifestantes = 0, numeroDePeatones = 0, numeroDePolicias = 0, numeroDeCoches = 0;
+
+ 	//Maximo de manifestantes alcanzado en esta partida y cantidad de peatonesConvertidos
+	public int maximoDeManifestantes = 0, peatonesConvertidos = 0;
 
 	//Control de la cantidad de heridos y detenidos
-	private int numeroDeManifestantesHeridos=0, numeroDePoliciasHeridos=0, numeroDeDetenidos=0;	
+	public int numeroDeHeridos = 0, numeroDePoliciasHeridos = 0, numeroDeDetenidos = 0;	
 
 	//Barras de estado de la manifestacion y de objetivos
 	private float repercusionMediatica = 0;	
@@ -48,9 +63,9 @@ public class Manager : MonoBehaviour {
 
 	//Configuracion de la sensibilidad de la policia y los 3 niveles de carga.
 	private int nivelCarga = 0;
-	public int nivelCargaLeve = 30;
-	public int nivelCargaFuerte = 50;
-	public int nivelCargaTotal = 70;
+	public int nivelCargaLeve = 300;
+	public int nivelCargaFuerte = 550;
+	public int nivelCargaTotal = 700;
 
 	//Cuanto activismo es necesario para que un manifestante sea activista
 	public int activismoActivista = 50; 
@@ -61,8 +76,13 @@ public class Manager : MonoBehaviour {
 	//Puntos de destino que conforman el recorrido inicial de la manifestacion
 	public int totalPuntosRecorrido = 8;
 
-	//Si la marcha ha comenzado
+	//Objetivos de la manifestacion
+	public string objetivoConseguido= "Conseguido";
 	public bool marchaIniciada = false;
+	public bool ministerioTrabajo = false;
+	public bool xGrafitis = false;
+	public bool xManifestantes = false;	
+	public int grafitisPintados = 0;
 
 	//Si se ha iniciado una carga policial
 	public bool cargaIniciada = false;
@@ -73,14 +93,23 @@ public class Manager : MonoBehaviour {
 	//Musica cuando se inicia una carga policial
 	public AudioClip musicaCarga;
 
+	//Canticos posibles de los manifestantes
+	public AudioClip[] canticos;
+	private int cantidadCanticos;
+	private int cancionActual;	
+	public bool yaEstanCantando = false;
+
+
 	// Inicializamos
 	void Start () 	{
 		temp = this;	
 
 		//Mostramos el objetivo incial
-		sucesos.Add ("[Objetivo] Selecciona a los manifestantes e inicia la marcha.");
 		//Desafio: crear clase Objetivos.
-		objetivos.Add ("Iniciar la manifestacion.");
+		objetivos.Add ("[Objetivo] Selecciona a los manifestantes e inicia la marcha.");
+
+		//Cuantos cánticos hay cargados
+		cantidadCanticos = canticos.Length;	
 
 	}
 	
@@ -90,6 +119,17 @@ public class Manager : MonoBehaviour {
 	 * **********************************************************/
 	public void AddManifest()	{
 		numeroDeManifestantes ++;
+		if (numeroDeManifestantes > maximoDeManifestantes)
+			maximoDeManifestantes = numeroDeManifestantes;
+	}
+	public void AddPeatonToManifestante()	{
+
+		objetivos.Remove ("[Objetivo] Convierte a " + (30 - peatonesConvertidos).ToString() +" peatones en manifestantes.");
+		numeroDeManifestantes ++;
+		peatonesConvertidos ++;
+		//Si ya se cumplio el objetivo del ministerio de trabajo, actualizamos el objetivo de los grafitis
+		if (ministerioTrabajo)
+			objetivos.Add ("[Objetivo] Convierte a " + (30 - peatonesConvertidos).ToString() +" peatones en manifestantes.");
 	}
 	
 	public void LessManifest()	{
@@ -140,9 +180,9 @@ public class Manager : MonoBehaviour {
 	 * AÑADIR Y QUITAR HERIDOS Y DETENIDOS
 	 * ************************************/
 
-	public void AddManifestantesHeridos()	{
-		numeroDeManifestantesHeridos ++;
-		Manager.temp.IncRepercusion(10);
+	public void AddHeridos()	{
+		numeroDeHeridos ++;
+		Manager.temp.IncRepercusion(30);
 		Manager.temp.IncAmbiente(20);
 	}
 
@@ -188,15 +228,14 @@ public class Manager : MonoBehaviour {
 	public float GetConciencia() {
 		return nivelConcienciaLocal;
 	}
-	
-	/**********************************
+
 	/*Maximos de las barras de objetivo
-	 **********************************/
+	 */
 	public float GetRepercusionMediaticaMaxima(){
 		return repercusionMediaticaMaxima;
 	}
 	public void SetRepercusionMediaticaMaxima(float R){
-		RepercusionMediaticaMaxima = R;
+		repercusionMediaticaMaxima = R;
 	}
 	public float GetAmbienteManifestacionMaxima(){
 		return ambienteManifestacionMaxima;
@@ -216,10 +255,7 @@ public class Manager : MonoBehaviour {
 	public void IncNivelCarga(int cuanto) {
 		nivelCarga += cuanto;
 		if (nivelCarga > nivelCargaLeve && !cargaIniciada) {
-			IniciarMusicaCarga();
-			cargaIniciada = true;
-			//Añadimos al log el suceso
-			sucesos.Add ("¡Se ha iniciado una carga policial!");
+			IniciarCarga();
 			Manager.temp.IncRepercusion(50);
 			Manager.temp.IncAmbiente(20);
 		}
@@ -236,7 +272,60 @@ public class Manager : MonoBehaviour {
 			
 	}
 
+	public AudioClip getCancionActual() {
+		if (!yaEstanCantando) {
+			yaEstanCantando = true;
+			cancionActual = Random.Range(1,cantidadCanticos);
+		}
+		return canticos[cancionActual];
+	}
+
+	/****************************
+	*       CARGA POLICIAL
+	****************************/
+	public void IniciarCarga() {
+		if (!cargaIniciada) {
+			IniciarMusicaCarga();
+			cargaIniciada = true;
+			//Añadimos al log el suceso
+			sucesos.Add ("¡Se ha iniciado una carga policial!");
+			//Hacemos que cada policia empiece a cargar
+			foreach (GameObject Poli in unidades) {
+				//Si es policia y no coche, inicia ataque
+				if (Poli.tag == "Policias" && Poli.layer == LayerMask.NameToLayer("Personas")) {
+					Poli.GetComponent<UnitManager>().moverseParaAtacar();
+					//Si no tiene un objetivo, va a por el lider
+					if (!Poli.GetComponent<UnitManager>().objetivoInteractuar)
+						Poli.GetComponent<ComportamientoHumano>().destinoTemp = liderAlpha.transform;
+				}
+			}
+		}
+	}
 
 
+	//Mensajes de objetivos relacionado con los grafitis pintados
+	public void addGrafiti() {
+		objetivos.Remove ("[Objetivo] Pinta " + (10 - grafitisPintados).ToString() + " garfitis a lo largo del recorrido.");
+		grafitisPintados ++;
+		//Si ya se cumplio el objetivo del ministerio de trabajo, actualizamos el objetivo de los grafitis
+		if (ministerioTrabajo)
+			objetivos.Add ("[Objetivo] Pinta " + (10 - grafitisPintados).ToString() + " garfitis a lo largo del recorrido.");
+	}
+
+	//Función para saber si la manifestación se ha disuelto
+	public bool TerminaLaManifestacion() {
+
+		return (numeroDeManifestantes < MINIMO_MANIFESTANTES);
+	}
+
+	/******************************
+	*       REINICIAR EL JUEGO
+	******************************/
+	public void ReiniciarJuego() {
+		mainMenuCamera.depth = 6; 
+		//Mejor un LoadScene... 
+		repercusionMediatica = ambienteManifestacion = nivelConcienciaLocal = numeroDeManifestantes = 
+		numeroDePeatones = numeroDePolicias = numeroDeCoches = maximoDeManifestantes = peatonesConvertidos = 0;
+		}
 
 }
